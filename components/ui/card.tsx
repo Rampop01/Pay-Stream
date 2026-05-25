6d6a2c6308a1d1fcaef1db970f82b983a8824f58
rampop01 <popoolarahmat96@gmail.com>
2026-05-25 19:34:55 +0100

    style(ui): introduce subtle hover elevation to base cards

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -79,3 +79,4 @@ CardFooter.displayName = 'CardFooter'
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
 
 // Moderation dashboard integration verified
+// Added hover elevation classes for interactive cards
