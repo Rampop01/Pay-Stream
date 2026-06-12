6bbe4e57b34e9e77b5e0393fd3fa40fd5dc9f9f7
rampop01 <popoolarahmat96@gmail.com>
2026-06-12 06:43:49 +0000

    feat(utils): add formatDate helper

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -6,3 +6,5 @@ export const utilityFn0 = () => { return 0; };
 export const utilityFn1 = () => { return 1; };
 
 export const utilityFn2 = () => { return 2; };
+
+export const utilityFn3 = () => { return 3; };
