b9a57a1b232098a3487e2acca11aa47bca1bd1c2
rampop01 <popoolarahmat96@gmail.com>
2026-06-17 19:00:10 +0000

    feat(hooks): implement useFirstMountState custom react hook

---
+++ hooks/utils/useFirstMountState.ts
@@ -0,0 +1,2 @@
+import { useRef } from 'react';
+export const useFirstMountState = () => { const isFirst = useRef(true); if (isFirst.current) { isFirst.current = false; return true; } return false; };
