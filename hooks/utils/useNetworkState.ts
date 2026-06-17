eef52db4c6feab782944e0c15109ea514cc2e404
rampop01 <popoolarahmat96@gmail.com>
2026-06-17 19:38:44 +0000

    feat(hooks): implement useNetworkState custom react hook

---
+++ hooks/utils/useNetworkState.ts
@@ -0,0 +1,2 @@
+import { useState, useEffect } from 'react';
+export const useNetworkState = () => { const [isOnline, setNetwork] = useState(true); useEffect(() => { setNetwork(navigator.onLine); }, []); return isOnline; };
